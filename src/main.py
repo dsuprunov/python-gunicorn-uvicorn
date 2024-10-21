1f722f75fb3beb676a2ae7e309b3a2e86b251a90
Dmitry Suprunov <dsuprunov@gmail.com>
2024-10-21 16:13:41 +0200

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+import uvicorn
+import pendulum
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def index():
+    return {
+        "now": pendulum.now(tz='UTC').format('YYYY-MM-DD HH:mm:ss z')
+    }
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
